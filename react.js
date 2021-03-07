f1d41ae044dee7494429f53a48c64768d2ff33af
zoy <409626581@qq.com>
2021-03-07 22:16:16 +0800

    fix: react config version

--- react.js
+++ react.js
@@ -1,31 +1,43 @@
 
-const path = require('path')
+const recursiveReaddir = require('recursive-readdir')
 
-const pkg = path.join(process.cwd(), 'package.json')
-const { react = 'detect' } = pkg.dependencies ?? {}
+module.exports = async function getReactVersion(){
+  let reactVersion = 'detect'
+  const files = await recursiveReaddir(process.cwd(),['node_modules',".*", "!package.json"])
 
-let reactVersion
-if (isNaN(Number(react.charAt(0))) && react !== 'detect') {
-  reactVersion = react.replace(react.charAt(0), '')
-} else {
-  reactVersion = react
-}
+  for(const file of files){
+    const pkg = require(file)
+    const { react } = pkg.dependencies ?? {}
 
-module.exports = {
-  extends: [
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended"
-  ],
-  rules:{
-    "react/prop-types": "off",
-    "react/jsx-handler-names": "off",
-    "react/jsx-fragments": "off",
-    "react/react-in-jsx-scope":"off",
-    "react/no-unused-prop-types": "off",
-  },
-  settings:  {
-      react: {
-        version: reactVersion
+    if (react){
+      if (isNaN(Number(react.charAt(0))) && react !== 'detect') {
+        reactVersion = react.replace(react.charAt(0), '')
+      } else {
+        reactVersion = react
       }
     }
-}
+    if (reactVersion !== 'detect'){
+      break
+    }
+  }
+  
+  return {
+    extends: [
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended"
+    ],
+    rules:{
+      "react/prop-types": "off",
+      "react/jsx-handler-names": "off",
+      "react/jsx-fragments": "off",
+      "react/react-in-jsx-scope":"off",
+      "react/no-unused-prop-types": "off",
+    },
+    settings:  {
+        react: {
+          version: reactVersion
+        }
+      }
+  }
+}()
+
--- test/test.js
+++ test/test.js
@@ -1,14 +1,52 @@
 
-const path = require('path')
+const recursiveReaddir = require('recursive-readdir')
 
-const pkg = path.join(process.cwd(), 'package.json')
-const { react = 'detect' } = pkg.dependencies ?? {}
 
-let reactVersion
-if (isNaN(Number(react.charAt(0))) && react !== 'detect') {
-  reactVersion = react.replace(react.charAt(0), '')
-} else {
-  reactVersion = react
-}
+module.exports = async function getReactVersion(){
+ 
+  let reactVersion = 'detect'
+  const files = await recursiveReaddir(process.cwd(),['node_modules',".*", "!package.json"])
+  // eslint-disable-next-line no-restricted-syntax
+  for(const file of files){
+    const pkg = require(file)
+    const { react } = pkg.dependencies ?? {}
 
-console.log(reactVersion);
+    if (react){
+      if (isNaN(Number(react.charAt(0))) && react !== 'detect') {
+        reactVersion = react.replace(react.charAt(0), '')
+      } else {
+        reactVersion = react
+      }
+    }
+    if (reactVersion !== 'detect'){
+      break
+    }
+  }
+  
+  return {
+    extends: [
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended"
+    ],
+    rules:{
+      "react/prop-types": "off",
+      "react/jsx-handler-names": "off",
+      "react/jsx-fragments": "off",
+      "react/react-in-jsx-scope":"off",
+      "react/no-unused-prop-types": "off",
+    },
+    settings:  {
+        react: {
+          version: reactVersion
+        }
+      }
+  }
+}()
+
+
+
+
+
+
+
+
