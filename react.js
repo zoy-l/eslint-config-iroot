6fb91de8883089a12903ca3c2a554bd86ae291d0
zoy <409626581@qq.com>
2021-03-09 02:45:10 +0800

    chore: add jsx rules

--- react.js
+++ react.js
@@ -4,7 +4,8 @@ module.exports = {
   extends: [
     isTs ? './typescript' : './base',
     'plugin:react/recommended',
-    'plugin:react-hooks/recommended'
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended'
   ],
   rules: {
     'react/prop-types': 'off',
